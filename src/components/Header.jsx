cf16922a0640b86fbe8b768183ce97f8127f5035
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-05-08 13:21:38 +0200

    header working

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="Header">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>RGA</li>
+          <li>Create Auction</li>
+          <li>Register</li>
+          <li>Log in</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Header;
