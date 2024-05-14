13910ca9f544e8adba368a01887dac6117f903f6
Sanne Delin <delinsanne@gmail.com>
2024-05-14 13:56:57 +0200

    backend connection login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from ".context/AuthContext";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import SingleAuction from "./components/SingleAuction";
@@ -14,18 +15,20 @@ function App() {
 
   /* blir med path variable i urlen som vi gjorde i postman */
   return (
-    <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route path="/createauction" exact element={<CreateAuction />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/userlogin" exact element={<UserLogin />} />
-          <Route path="/profile" exact element={<Profile />} />
-          <Route path="/registeruser" exact element={<RegisterUser />} />
-          <Route path="/singleauction" exact element={<SingleAuction />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/createauction" exact element={<CreateAuction />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/userlogin" exact element={<UserLogin />} />
+            <Route path="/profile" exact element={<Profile />} />
+            <Route path="/registeruser" exact element={<RegisterUser />} />
+            <Route path="/singleauction" exact element={<SingleAuction />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
