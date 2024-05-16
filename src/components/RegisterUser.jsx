72a0d8d465b16d0a3e3fd12a73989fbe4cebce48
RicPet87 <richard_pettersson@hotmail.com>
2024-05-16 05:12:20 +0200

    basics done

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const RegisterUser = () => {
-    return(
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!username || !email || !password) {
+            alert("Fill in username, email and password!");
+            return;
+        }
+
+        try {
+            const { data } = await axios.post(
+              `${import.meta.env.VITE_API_URL}/auth/signup`,
+              {
+                username,
+                email,
+                password,
+              },
+              { withCredentials: true }
+            );
+            return navigate("/");
+        } catch (err){
+            console.log("Error: " + err);
+        }
+
+
+
+
+
+    };
+
+
+
+
+
+    return (
          <div>
             <h1>RgaLogo</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
 
             <div>
                 <label>Username</label>
