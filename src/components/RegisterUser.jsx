5636bbe706aa224a0f91f2ac0c31c9c9eb47ed36
RicPet87 <richard_pettersson@hotmail.com>
2024-05-16 11:05:42 +0200

    able to succesfully create an account. needs some polish

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+
 
 const RegisterUser = () => {
 
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     const navigate = useNavigate();
 
@@ -19,22 +20,27 @@ const RegisterUser = () => {
         }
 
         try {
-            const { data } = await axios.post(
-              `${import.meta.env.VITE_API_URL}/auth/signup`,
-              {
+            const response = await fetch("http://localhost:8080/api/auth/signup", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
                 username,
                 email,
                 password,
-              },
-              { withCredentials: true }
-            );
-            return navigate("/");
-        } catch (err){
-            console.log("Error: " + err);
-        }
+              }),
+            });
 
-
-
+            if (response.ok){
+                navigate("/signin")
+            } else {
+                setError("Signup failed")
+            } 
+        } catch (error) {
+            console.error("Error:", error);
+            setError("An error occured")
+        }    
 
 
     };
@@ -52,7 +58,9 @@ const RegisterUser = () => {
                 <label>Username</label>
                 <input
                     type="text"
-                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
                 />
             </div>
 
@@ -60,7 +68,9 @@ const RegisterUser = () => {
                 <label>Email</label>
                 <input
                     type="email"
-                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
                 />   
             </div>
 
@@ -68,7 +78,9 @@ const RegisterUser = () => {
                 <label>Password</label>
                 <input
                     type="password"
-                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
                 />   
             </div>
             <button type="submit">Register</button>
