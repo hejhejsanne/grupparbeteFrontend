e761433856e479210130d2e681171f70f40f9d97
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-05-20 12:09:46 +0200

    doing some testing

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,36 @@ import SingleAuction from "./components/SingleAuction";
 import RegisterUser from "./components/RegisterUser";
 import CreateAuction from "./components/CreateAuction";
 import UserLogin from "./components/UserLogin";
+import AuctionCard from "./components/AuctionCard";
 
 function App() {
   const [count, setCount] = useState(0);
+  const AuctionCard = ({ auction }) => {
+    const { picture, endDate, highestBid } = auction;
 
-  /* blir med path variable i urlen som vi gjorde i postman */
-  return (
-    <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route path="/createauction" exact element={<CreateAuction />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/userlogin" exact element={<UserLogin />} />
-          <Route path="/profile" exact element={<Profile />} />
-          <Route path="/registeruser" exact element={<RegisterUser />} />
-          <Route path="/singleauction" exact element={<SingleAuction />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    /* blir med path variable i urlen som vi gjorde i postman */
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/createauction" exact element={<CreateAuction />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/userlogin" exact element={<UserLogin />} />
+            <Route path="/profile" exact element={<Profile />} />
+            <Route path="/registeruser" exact element={<RegisterUser />} />
+            <Route path="/singleauction" exact element={<SingleAuction />} />
+          </Routes>
+          <div className="auction-card">
+            <img src={picture} alt="Auction" />
+            <div>
+              <p>End Date: {endDate}</p>
+              <p>Highest Bid: {highestBid}</p>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  };
 }
 
 export default App;
--- src/components/AuctionCard.jsx
+++ src/components/AuctionCard.jsx
@@ -1,52 +1,35 @@
-import { useContext } from "react";
-import testBild from "../assets/images/testBild.jpg";
-import { AuctionCardContext } from "../context/AuctionCardContext";
 
-const AuctionCard = ({ auction }) => {
-  // const [img, setImg] = useState("");
-  // const [title, setTitle] = useState("");
-  // const [endDate, setEndDate] = useState("");
-  // Separate state variables, ist this better to use or not?
 
-  // const { auction } = useContext(AuctionCardContext);
+import { useState, useEffect } from "react";
+import AuctionCard from "./AuctionCard";
+import axios from "axios";
 
-  //   singel object props
-  const { image, title, endDate } = auction;
+const Home = () => {
+  const [auctions, setAuctions] = useState([]);
 
-  //   error handler that puts in another photo if there are none
-  const handleImageError = (event) => {
-    event.target.src = "/default-auction-image.jpg";
-  };
+  useEffect(() => {
+    // Fetch auctions data
+    const fetchAuctions = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/auction/add"
+        );
+        setAuctions(response.data);
+      } catch (error) {
+        console.error("Error fetching auctions:", error);
+      }
+    };
 
-  //const formatEndDate = new Date(endDate).toLocaleDateString();
-  // this can be used to format the end date for display if we dont have it allready in backend
-  //   if this is going to be used, put {formatEndDate} insted of {endDate} in the p tag
+    fetchAuctions();
+  }, []);
 
   return (
-    <div className="container">
-      <div className="card-box">
-        <div className="image image1">
-          <img src={image} alt="picture" onError={handleImageError} />
-        </div>
-        <div>
-          <h3>{title}</h3>
-          <p>{endDate}</p>
-        </div>
-      </div>
+    <div>
+      {auctions.map((auction) => (
+        <AuctionCard key={auction.id} auction={auction} />
+      ))}
     </div>
   );
 };
 
 export default AuctionCard;
-
-// this is my hardcoded box that worked!
-
-// {/* <div className="container">
-//       <div className="card-box">
-//         <div className="image image1">
-//           <img src={testBild} alt="spelkonsoll" />
-//         </div>
-//         <div className="title">nintendo 64</div>
-//         <div className="endDate"> 7/8 kl 12.00</div>
-//       </div>
-//     </div> */}
